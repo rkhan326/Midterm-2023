d7c895a1aec9045a493d78148460371f5fd5b5ec
rkhan326 <rkhan326@gmail.com>
2023-04-10 21:36:03 -0400

    anagram, permutation, palindrome duplicateword

--- src/math/problems/FindMissingNumber.java
+++ src/math/problems/FindMissingNumber.java
@@ -1,6 +1,6 @@
 package math.problems;
 
-public class FindMissingNumber {
+public class FindMissingNumberMain {
 
     public static void main(String[] args) {
         /*
@@ -8,7 +8,32 @@ public class FindMissingNumber {
          * For example {10, 2, 1, 4, 5, 3, 7, 8, 6}. One number will be missing in array (9 in this case).
          * Write java code to find the missing number from the array. Write static helper method to find it.
          */
-         int [] array = new int[]{10, 2, 1, 4, 5, 3, 7, 8, 6};
+
+        int[] array = new int[]{10, 2, 1, 4, 5, 3, 7, 9, 6};
+        System.out.println(findMissingNumber(array));
 
     }
+    public static int findMissingNumber(int array1[]) {
+        int N = array1.length;
+        int sumGivenArrayElements = 0;
+        int sumAllArrayElements = 0;
+        int missingNumber = 20;
+
+        sumAllArrayElements = ((N + 1) * (N + 2)) / 2;
+
+        for (int i = 0; i < N; i++) {
+            sumGivenArrayElements = sumGivenArrayElements + array1[i];
+        }
+        missingNumber = sumAllArrayElements - sumGivenArrayElements;
+        return missingNumber;
+    }
 }
+
+
+
+
+
+
+
+
+
